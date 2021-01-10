17f57c40b5d25348af71d247cd5a2e2eab112f0b
lil-jojo <littlejomiles@gmail.com>
2021-01-10 17:47:26 +0000

    Implemented an abstract coordinate system.

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ window.addEventListener("resize", setScreenSize)
 
 window.requestAnimationFrame(gameLoop)
 
-let x
-let y
+let x = 40
+let y = 15
+let absX
+let absY
 let width = 10
 let height = 10
 setScreenSize()
 
 function draw() {
 	screen.fillStyle = "#FFFF00"
-	screen.fillRect(x, y, width, height)
+	screen.fillRect(absX-(width/2), absY-(height/2), width, height)
 }
 
 function gameLoop() {
@@ -26,6 +28,17 @@ function setScreenSize(){
 	canvas.height = canvas.width / 2.031
 	screen.fillStyle = "palevioletred"
 	screen.fillRect(0, 0, canvas.width, canvas.height)
-	x = canvas.width / 2
-	y = canvas.height / 2
+	console.log("W:", canvas.width, "H:", canvas.height)
+	// x = canvas.width / 2
+	// y = canvas.height / 2
+	// console.log( "0,0", map(0,0), x,y )
+	absX = map(x,y).x
+	absY = map(x,y).y
 }
+
+function map(x, y){ 
+	let newX = ((x+50)/100) * canvas.width
+	let newY = ((y+50)/100) * canvas.height
+	return {x:newX, y:newY}
+}
+
