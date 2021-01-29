2bf06fe3edafc236a5144b34fd4a74a92ea63e89
lil-jojo <littlejomiles@gmail.com>
2021-01-29 18:28:59 +0000

    Made square move across the screen

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const canvas = document.getElementById("canvas")
+const display = document.querySelector("h1")
+// console.log(typeof display.innerHTML)
+// display.innerHTML
+console.log("DISP", display)
 const screen = canvas.getContext("2d")
 
 window.addEventListener("resize", setScreenSize)
 
 window.requestAnimationFrame(gameLoop)
 
-let x = 40
-let y = 15
+let x = 0
+let y = 0
 let absX
 let absY
 let width = 10
 let height = 10
+let direction = "l"
+let speed = 50/4/1000 // how many units to move per millisecond
+let lastFrameTime = performance.now()
 setScreenSize()
 
 function draw() {
-	screen.fillStyle = "#000000"
+	screen.fillStyle = "yellow"
+	screen.fillRect(0, 0, canvas.width, canvas.height)
+	screen.fillStyle = "DeepPink"
 	screen.fillRect(absX-(width/2), absY-(height/2), width, height)
 }
 
+function move(el) {
+	x = x + (speed * el)
+	absX = map(x,y).x
+	absY = map(x,y).y
+}
+
 function gameLoop() {
+	const elapsed = performance.now()-lastFrameTime
+	display.innerHTML = elapsed
+	// display.innerHTML("underpants")
+	move(elapsed)
 	draw()
+	lastFrameTime = performance.now()
 	window.requestAnimationFrame(gameLoop)
 }
 
@@ -29,9 +49,6 @@ function setScreenSize(){
 	screen.fillStyle = "yellow"
 	screen.fillRect(0, 0, canvas.width, canvas.height)
 	console.log("W:", canvas.width, "H:", canvas.height)
-	// x = canvas.width / 2
-	// y = canvas.height / 2
-	// console.log( "0,0", map(0,0), x,y )
 	absX = map(x,y).x
 	absY = map(x,y).y
 }
